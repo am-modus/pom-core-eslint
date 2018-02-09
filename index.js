b5ba698a890800a2aa1dbec0c9940423fc8cf365
Asim Mittal <asim.mittal@gmail.com>
2018-02-09 17:45:25 -0500

    committing es lint config

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": "airbnb-base",
+    rules:{
+        "linebreak-style": 0,
+        "semi": ["error", "always"],
+        "indent": [2, 4],
+        "comma-dangle": 0,
+        'class-methods-use-this': 0,
+        'no-unused-vars': 0,
+        'arrow-body-style': ['error', 'always'],
+        "prefer-destructuring": 0
+      },
+};
