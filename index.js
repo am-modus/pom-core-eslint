54e9d1c8cd270256c58b0eca67c8ba2093556ea2
Asim Mittal <asim.mittal@gmail.com>
2018-02-09 18:46:44 -0500

    adding no-console

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 module.exports = {
-    "extends": "airbnb-base",
+    'extends': 'airbnb-base',
     rules:{
-        "linebreak-style": 0,
-        "semi": ["error", "always"],
-        "indent": [2, 4],
-        "comma-dangle": 0,
+        'linebreak-style': 0,
+        'semi': ['error', 'always'],
+        'indent': [2, 4],
+        'comma-dangle': 0,
         'class-methods-use-this': 0,
         'no-unused-vars': 0,
         'arrow-body-style': ['error', 'always'],
-        "prefer-destructuring": 0
+        'prefer-destructuring': 0,
+        'no-console': ['error', { allow: ['warn', 'error'] }]
       },
 };
